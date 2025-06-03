4039313ac8dce8ed15915af8b8261eaff8616ec5
jamal531 <j44003183@gmail.com>
2025-06-03 15:46:14 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    signals = [
+        {"coin": "BTC/USDT", "price": "$67,312", "signal": "STRONG BUY", "strength": "Very High", "timeframe": "1 Minute"},
+        {"coin": "ETH/USDT", "price": "$3,240", "signal": "WAIT", "strength": "Low", "timeframe": "3 Minutes"},
+        {"coin": "BNB/USDT", "price": "$580", "signal": "STRONG SELL", "strength": "Very High", "timeframe": "Hourly"},
+        {"coin": "DOGE/USDT", "price": "$0.147", "signal": "BUY", "strength": "Medium", "timeframe": "1 Minute"},
+    ]
+    return render_template('index.html', signals=signals)
+
+if __name__ == '__main__':
+    app.run(debug=True)
