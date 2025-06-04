0c68d2f2ae2e035be46dea06e60c4ce9affdef5d
jamal531 <j44003183@gmail.com>
2025-06-04 20:53:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 
 from flask import Flask, render_template
-import datetime
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    now = datetime.datetime.now()
-    return render_template('index.html', time=now.strftime("%H:%M:%S"))
+    current_time = datetime.now().strftime('%H:%M:%S')
+    return render_template('index.html', time=current_time)
 
 if __name__ == '__main__':
-    import os
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
