abbd8af43aeeb914a75a179a6cbca45299daf665
jamal531 <j44003183@gmail.com>
2025-06-04 20:41:48 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    now = datetime.datetime.now()
+    return render_template('index.html', time=now.strftime("%H:%M:%S"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
